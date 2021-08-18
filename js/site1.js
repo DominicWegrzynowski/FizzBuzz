1626c47477a5aeae4104d3b6f444fb81dd89678e
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2021-08-18 15:36:16 -0400

    v4

--- js/site1.js
+++ js/site1.js
@@ -1,4 +1,5 @@
 function getValues() {
+    
     let fizzValue = parseInt(document.getElementById("fizzValue").value);
     let buzzValue = parseInt(document.getElementById("buzzValue").value);
 
@@ -8,7 +9,9 @@ function getValues() {
         displayData(fizzBuzzArray);
 
     } else {
+
         alert("You must enter an integer");
+
     }
 
 }
@@ -49,19 +52,35 @@ function displayData(fizzBuzzArray) {
     
     //get the table body element from the page
     let tableBody = document.getElementById("results");
+
     //get the template itsself
     let templateRow = document.getElementById("fizzBuzzTemplate");
+
     //clear table
     tableBody.innerHTML = "";
     
-    for (let index = 0; index < fizzBuzzArray.length; index += 5) {
+    for (let i = 0; i < fizzBuzzArray.length; i += 5) {
 
         let tableRow = document.importNode(templateRow.content, true);
         
-        //grab just the tds and put into an array
-        let rowCols = tableRow.querySelector("td");
+        let rowCols = tableRow.querySelectorAll("td");
+
+        rowCols[0].classList.add(fizzBuzzArray[i]);
         rowCols[0].textContent = fizzBuzzArray[i];
-        
+
+        rowCols[1].classList.add(fizzBuzzArray[i + 1]);
+        rowCols[1].textContent = fizzBuzzArray[i + 1];
+
+        rowCols[2].classList.add(fizzBuzzArray[i + 2]);
+        rowCols[2].textContent = fizzBuzzArray[i + 2];
+
+        rowCols[3].classList.add(fizzBuzzArray[i + 3]);
+        rowCols[3].textContent = fizzBuzzArray[i + 3];
+
+        rowCols[4].classList.add(fizzBuzzArray[i + 4]);
+        rowCols[4].textContent = fizzBuzzArray[i + 4];
+
+        tableBody.appendChild(tableRow);
     }
     
 }
